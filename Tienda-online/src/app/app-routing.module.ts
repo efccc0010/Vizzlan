fc4db227657308498b9191886241eae427210b8f
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-02 19:36:02 -0500

    Rutas angular

--- Tienda-online/src/app/app-routing.module.ts
+++ Tienda-online/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './Vistas/inicio/inicio.component';
+import { ProductosComponent } from './Vistas/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:InicioComponent},
+  {path:'Productos',component:ProductosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tienda-online/src/app/app.module.ts
+++ Tienda-online/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './Vistas/inicio/inicio.component';
+import { ProductosComponent } from './Vistas/productos/productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    InicioComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
