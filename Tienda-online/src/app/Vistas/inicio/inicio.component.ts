415728c86d7e00c4d74c1ab90ae014cea4a8180d
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-03 01:44:59 -0500

    card

--- Tienda-online/src/app/Vistas/inicio/inicio.component.ts
+++ Tienda-online/src/app/Vistas/inicio/inicio.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
-
-  constructor() { }
+  public cards: any;
+  constructor() { 
+    this.cards=[
+      {
+        UrlImagen:'https://home.ripley.com.pe/Attachment/WOP_5/2015255773591/2015255773591_2.jpg'
+      },
+      {
+        UrlImagen:'https://home.ripley.com.pe/Attachment/WOP_5/2015265119563/2015265119563-2.jpg'
+      },
+  ]
+  }
 
   ngOnInit(): void {
   }
