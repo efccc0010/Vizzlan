5dbe96c98229b6ff1f62253f86e310e86c98dd65
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-03 02:25:09 -0500

    nav-bar

--- Tienda-online/src/app/app.module.ts
+++ Tienda-online/src/app/app.module.ts
@@ -3,20 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+
 import { InicioComponent } from './Vistas/inicio/inicio.component';
 import { ProductosComponent } from './Vistas/productos/productos.component';
 import { CardComponent } from './elements/card/card.component';
 import { SliderCardComponent } from './elements/slider-card/slider-card.component';
+import { NavBarComponent } from './elements/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     InicioComponent,
     ProductosComponent,
     CardComponent,
-    SliderCardComponent
+    SliderCardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
