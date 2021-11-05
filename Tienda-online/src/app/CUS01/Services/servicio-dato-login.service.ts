43274247a569224a27c3b6fdf889c1d379a16e90
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-04 21:34:12 -0500

    Habilitadas opcion pedido especial

--- Tienda-online/src/app/CUS01/Services/servicio-dato-login.service.ts
+++ Tienda-online/src/app/CUS01/Services/servicio-dato-login.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ServicioDatoLoginService {
-  nombreUsuario:String=''
+  nombreUsuario:any=null
   constructor() { }
 }
