d8c75e1afb0d554a6584812d91b4698adb489bb3
Patrick Huillca <patrick.h291299@gmail.com>
2021-11-05 01:42:50 -0500

    carrusel

--- Tienda-online/src/app/Vistas/inicio/inicio.component.ts
+++ Tienda-online/src/app/Vistas/inicio/inicio.component.ts
@@ -7,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
   public cards: any;
+  public ImgCarr:any;
   constructor() { 
-    
+    this.ImgCarr=[
+      {url:'./assets/imagenes/Tipojeans1.jpeg',
+      clase:'carousel-item active'},
+    {url:'./assets/imagenes/Tipojeans2.jpg',
+     clase:'carousel-item'},
+    {url:'./assets/imagenes/Tipojeans3.jpeg',
+    clase:'carousel-item'},
+    {url:'./assets/imagenes/Tipojeans4.jpg',
+    clase:'carousel-item'}]
   }
 
   ngOnInit(): void {
