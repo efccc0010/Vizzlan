b6bc6cb9c66b8ace670192db1b1cfc1c2f3ed548
fernandoedgar <edgar.carrion.c@uni.pe>
2021-11-11 07:16:22 -0500

    Agregando visualizar-decoraciones

---
+++ Tienda-online/src/app/CUS15/visualizar-decoraciones/visualizar-decoraciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-decoraciones',
+  templateUrl: './visualizar-decoraciones.component.html',
+  styleUrls: ['./visualizar-decoraciones.component.scss']
+})
+export class VisualizarDecoracionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
